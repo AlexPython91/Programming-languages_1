aff00e1058264da0c0ab3e67e99118b07926d6a0
alexhvorostov <hvorostov.alex1@gmail.com>
2021-10-24 19:52:35 +0400

    add Example 006 if/else

---
+++ Example006_ifElse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")  // ToLower приводим ответ к верхнему регистру.
+{
+    Console.WriteLine("Ура, Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
