a229e8b3ed848e3f0918ebdc8f19a76b79ded153
alexhvorostov <hvorostov.alex1@gmail.com>
2021-10-24 16:44:52 +0400

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+int numberA = 31;
+int numberB = 50;
+int result = numberA + numberB;    // Доп шаг. Можно без него
+
+Console.WriteLine(result);   // без result пишем просто (А + В)
+
