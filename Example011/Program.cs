e53398d384483cafee7ab92f2022209310847191
alexhvorostov <hvorostov.alex1@gmail.com>
2021-10-27 00:00:00 +0400

    add Example 011

---
+++ Example011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int [] array = {1, 12, 31, 18, 4, 15, 16, 17, 18};
+
+int n = array.Length;  // кол-во элементов массива
+int find = 18;  // загадываем искомое число
+
+int index = 0; // установливаем индекс
+
+while (index < n)   // Цикл пока индекс меньше n
+{
+    if (array[index] == find)   // условие индекс элемента равен искомому
+    {
+        Console.WriteLine(index);
+        break;     // если условие выполняется
+    }
+    index++;
+}
